5234254ec5e1c6586d07ff8ae75dfd40ac59a896
Augustine Dunn <wadunn83@gmail.com>
2012-03-17 14:59:28 -0700

    Initial commit after project creation using modern-package-template with minimal info added during the set up script.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='gFunc',
+    version=version,
+    description="Build functional genomic gene sets by incorporating data from disperate OMICS data-streams using graphs.",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='bioinformatics genomics graphs transcriptomics genomics genesets',
+    author='Augustine Dunn',
+    author_email='wadunn83@gmail.com',
+    url='http://scipher.wordpress.com/',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['gFunc=gfunc:main']
+    }
+)
