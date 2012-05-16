c172b144792f7354e017828215ac82d60bc96c50
Augustine Dunn <wadunn83@gmail.com>
2012-05-15 22:26:51 -0700

    /src/gfunc/graphTools.py: - empty class GraphBuilder()

--- src/gfunc/graphTools.py
+++ src/gfunc/graphTools.py
@@ -4,3 +4,10 @@ graphTools.py
 ####################
 Code supporting building and querying the graphs.
 """
+
+class GraphBuilder(object):
+    """
+    """
+    def __init_(self):
+        """
+        """
