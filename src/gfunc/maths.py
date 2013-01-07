9419160d25efd81ccda1d3ee0089fb0e20f11b02
Augustine Dunn <wadunn83@gmail.com>
2013-01-07 14:47:20 -0800

    src/gfunc/maths.py: - docstrings: ammended and modtified for rst formatting

--- src/gfunc/maths.py
+++ src/gfunc/maths.py
@@ -10,13 +10,13 @@ import numpy as np
 
 def bayesian_score(c,m,n,scores,scale_mod=1):
     """
-    BS = ((c * m) + sum([x for x in scores])) / (n + c)
-    
-    Where:
-    
-    n: number of votes for THIS item
-    C: median number of votes for all items that got at least 1 vote (weighting or dampening factor)
-    m: median UNweighted score for all items that got at least 1 vote  
+    | BS = ((c * m) + sum([x for x in scores])) / (n + c)
+    | 
+    | Where:
+    | 
+    | ``n``: number of votes for THIS item
+    | ``C``: median number of votes for all items that got at least 1 vote (weighting or dampening factor)
+    | ``m``: median UNweighted score for all items that got at least 1 vote  
     """
     c = float(c)
     
