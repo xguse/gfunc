37173dda4882b6df11f7019c8ce353a077a2bab4
Augustine Dunn <wadunn83@gmail.com>
2013-01-07 14:17:14 -0800

    DOCS: - spent time updating my docstrings and formatting them for ReST - rebuilt the docs a few times

--- doc/code.rst
+++ doc/code.rst
@@ -4,17 +4,73 @@ gFunc Auto-Generated Code Documentation
 .. automodule:: gfunc.__init__
     :members:
 
-.. automodule:: gfunc.fileIO
-    :members:
-	
-.. automodule:: gfunc.fileIO.parsers
+.. automodule:: gfunc.parsers.__init__
     :members:
 
-.. automodule:: gfunc.galaxy_tools
+    .. automodule:: gfunc.parsers.base
+        :members:
+        
+    .. automodule:: gfunc.parsers.Cufflinks
+        :members:
+        
+    .. automodule:: gfunc.parsers.edge_lists
+        :members:
+    
+    .. automodule:: gfunc.parsers.ETE
+        :members:
+    
+    .. automodule:: gfunc.parsers.GTF
+        :members:
+    
+    .. automodule:: gfunc.parsers.JASPAR
+        :members:
+    
+    .. automodule:: gfunc.parsers.MAST
+        :members:
+
+
+
+
+.. automodule:: gfunc.analysis_classes
     :members:
 
+.. automodule:: gfunc.clustering
+    :members:
+    
 .. automodule:: gfunc.data_classes
     :members:
-	
+
+.. automodule:: gfunc.ensembl_data
+    :members:
+
+.. automodule:: gfunc.errors
+    :members:
+
+.. automodule:: gfunc.externals
+    :members:
+
+.. automodule:: gfunc.fdr
+    :members:
+
+.. automodule:: gfunc.fileIO
+    :members:
+
 .. automodule:: gfunc.graphTools
+    :members:
+
+.. automodule:: gfunc.maths
+    :members:
+
+.. automodule:: gfunc.motifs
+    :members:
+
+.. automodule:: gfunc.stats
+    :members:
+
+.. automodule:: gfunc.xpermutations
+    :members:
+
+
+
+.. automodule:: gfunc.galaxy_tools
     :members:
--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,17 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('../src/'))
 
+
+# -- Custom helper functions ---------------------------------------------------
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -70,15 +81,15 @@ exclude_patterns = ['_build']
 #default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
+add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#add_module_names = True
+add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
-#show_authors = False
+show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -150,10 +161,10 @@ html_static_path = ['_static']
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
