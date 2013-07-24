7f0ec5ee23c9a14a88f15ba62039aecbda3f15ee
Gus Dunn <wadunn83@gmail.com>
2013-07-23 20:27:39 -0700

    2013-07-23 2028: restored src/gfunc/devel.py from branch sane_safe

--- src/gfunc/devel.py
+++ src/gfunc/devel.py
@@ -10,6 +10,7 @@ import numpy as np
 from scipy import stats as sp_stats
 
 from gfunc.maths import weight_d_for_ptci as scale_the_d
+from gfunc.data_classes import Bunch
 
 
 def is_none_or_nan(value):
@@ -52,12 +53,17 @@ def edge_correlation(gFunc_edge):
         if """'Bunch' object has no attribute""" in err.message:
             # if this executes then one of the nodes did not have an expression_vector which means no r is possible
             # in this case return None
+            gFunc_edge.data.r_val = None
+            gFunc_edge.data.p_val = None
             return None
         else:
             # if this executes then something ELSE went wrong: thus I will fail.
             raise err
 
-def calc_ptci_rpd(gFunc_edge):
+
+######################## START CALC  PTCI LAND ######################################
+
+def calc_ptci_rpd(gFunc_edge,w_min=1.0,w_max=1.1):
     """
     calculate the PTCI
     edge_correlation() should already have been run
@@ -67,88 +73,97 @@ def calc_ptci_rpd(gFunc_edge):
         p_val = gFunc_edge.data.p_val
         d_val,d_min,d_max = gFunc_edge.data.divergence
         
-        return r_val * (1-p_val) * scale_the_d(d_val,d_min,d_max) 
+        return r_val * (1-p_val) * scale_the_d(d_val,d_min,d_max,w_min,w_max) 
     
-    except TypeError as err:
-        if str(err) == "'NoneType' object is not iterable":
-            # If we get an invalid ptci, store it and return it as None
-            gFunc_edge.data.PTCI = None
-            return None
-        else:
-            raise
+    except (TypeError,AttributeError):
+        return None
 
-def calc_ptci_rd(gFunc_edge):
+
+def calc_ptci_rd(gFunc_edge,w_min=1.0,w_max=1.1):
     """
     calculate the PTCI
     edge_correlation() should already have been run
     """
-    try:
-        d_val,d_min,d_max = gFunc_edge.data.divergence  
+    try:  
         r_val = gFunc_edge.data.r_val
-
-        return r_val * scale_the_d(d_val,d_min,d_max) 
+        d_val,d_min,d_max = gFunc_edge.data.divergence
+        
+        return r_val * scale_the_d(d_val,d_min,d_max,w_min,w_max) 
     
-    except TypeError as err:
-        if str(err) == "'NoneType' object is not iterable":
-            # If we get an invalid ptci, store it and return it as None
-            gFunc_edge.data.PTCI = None
-            return None
-        else:
-            raise
+    except (TypeError,AttributeError):
+        return None
+        
+def calc_ptci_zd(gFunc_edge,w_min=1.0,w_max=1.1):
+    """
+    calculate the PTCI
+    edge_correlation() should already have been run
+    """
+    try:  
+        z_val = gFunc_edge.data.z_val
+        d_val,d_min,d_max = gFunc_edge.data.divergence
+        
+        return z_val * scale_the_d(d_val,d_min,d_max,w_min,w_max) 
+    
+    except (TypeError,AttributeError):
+        return None
 
-def calc_ptci_zpd(gFunc_edge):
+def calc_ptci_zpd(gFunc_edge,w_min=1.0,w_max=1.1):
     """
     calculate the PTCI
     edge_correlation() should already have been run
     """
     try:
-        d_val,d_min,d_max = gFunc_edge.data.divergence  
         z_val = gFunc_edge.data.z_val
         p_val = gFunc_edge.data.p_val
-
-        return z_val * (1-p_val) * scale_the_d(d_val,d_min,d_max) 
+        d_val,d_min,d_max = gFunc_edge.data.divergence
+        
+        return z_val * (1-p_val) * scale_the_d(d_val,d_min,d_max,w_min,w_max) 
     
-    except TypeError as err:
-        if str(err) == "'NoneType' object is not iterable":
-            # If we get an invalid ptci, store it and return it as None
-            gFunc_edge.data.PTCI = None
-            return None
-        else:
-            raise
+    except (TypeError,AttributeError):
+        return None
 
-def calc_ptci_z(gFunc_edge):
+def calc_ptci_z(gFunc_edge,w_min=1.0,w_max=1.1):
     """
     calculate the PTCI
     edge_correlation() should already have been run
     """
     try:
         z_val = gFunc_edge.data.z_val
-
 
         return z_val 
     
-    except TypeError as err:
-        if str(err) == "'NoneType' object is not iterable":
-            # If we get an invalid ptci, store it and return it as None
-            gFunc_edge.data.PTCI = None
-            return None
-        else:
-            raise
+    except (TypeError,AttributeError):
+        return None
 
-def get_ptci(gFunc_edge,use_p=False,kind='rpd'):
+def calc_ptci_r(gFunc_edge,w_min=1.0,w_max=1.1):
+    """
+    calculate the PTCI
+    edge_correlation() should already have been run
+    """
+    try:
+        r_val = gFunc_edge.data.r_val
+
+        return r_val 
+    
+    except (TypeError,AttributeError):
+        return None
+
+######################## END CALC PTCI LAND ######################################
+
+
+
+def get_ptci(gFunc_edge,kind='rpd',w_min=1.0,w_max=1.1):
     """
     calculate and store 
     """
-    which_ptci = {'rpd':,
-                  'zpd':,
-                  'rd':,
-                  'zd':,
-                  'r':,
-                  'z':}
-    if use_z:
-        ptci = calc_ptci_z(gFunc_edge,use_p)
-    else:
-        ptci = calc_ptci(gFunc_edge,use_p)
+    ptci_kind = Bunch({ 'rpd' : calc_ptci_rpd,
+                        'zpd' : calc_ptci_zpd,
+                        'rd'  : calc_ptci_rd,
+                        'zd'  : calc_ptci_zd,
+                        'r'   : calc_ptci_r,
+                        'z'   : calc_ptci_z })
+    
+    ptci = ptci_kind[kind](gFunc_edge,w_min,w_max)
     
     if not is_none_or_nan(ptci):
         # If we get a valid ptci store the value in the gFunc_edge object and also return it
