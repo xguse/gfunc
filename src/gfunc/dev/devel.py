08e7d33f174978a1b5225fd118734cb7616fb317
Gus Dunn <wadunn83@gmail.com>
2013-09-03 11:43:42 -0700

    2013-09-03 1143: commiting changes including transfer of defs from ipython notebooks to dev.devel and dev.motifs

--- src/gfunc/dev/devel.py
+++ src/gfunc/dev/devel.py
@@ -2,17 +2,24 @@
 ####################
 devel.py
 ####################
-Code under development before being placed in a logical location.
+Code under elopment before being placed in a logical location.
 """
 
 import numpy as np
 import pandas
 
+from collections import defaultdict
+
 from scipy import stats as sp_stats
 
 from gfunc.maths import weight_d_for_ptci as scale_the_d
+from gfunc import maths as m
 from gfunc.data_classes import Bunch
+from gfunc import xpermutations
 
+
+# Clever trick to create endlessly dimentional nested dicts on the fly
+dict_tree = lambda: defaultdict(dict_tree)
 
 def is_none_or_nan(value):
     """
@@ -60,6 +67,182 @@ def edge_correlation(gFunc_edge):
         else:
             # if this executes then something ELSE went wrong: thus I will fail.
             raise err
+
+
+
+def get_edge_r_and_p_vals(edges,quiet=True):
+    """
+    set and get r and p vals from list of edges
+    """
+    # collect all the results using edge_correlation()
+    edge_r_and_p_values = [edge_correlation(edge) for edge in edges]
+    
+    if not quiet:
+        print "r_vals before cleaning: %s" % (len(edge_r_and_p_values))
+
+    # get rid of any results that equal None
+    edge_r_and_p_values = [x for x in edge_r_and_p_values if not is_none_or_nan(x)]
+    
+    if not quiet:
+        print "Returning %s r_vals." % (len(edge_r_and_p_values))
+        
+    return edge_r_and_p_values
+
+def get_pairwise_ptci_vals(edges,kind='rpd',quiet=True,w_min=1.0,w_max=1.1):
+    """
+    Function to calculate and store the 1-to-1 pairwise PTCI values in the graph edges
+    """
+    if not quiet:
+        print "kind: %s" % (kind)
+    pairwise_ptci_vals = [get_ptci(edge,kind,w_min,w_max) for edge in edges]
+    if not quiet:
+        print "ptci_vals before cleaning: %s" % (len(pairwise_ptci_vals))
+    # remove any None values
+    pairwise_ptci_vals = [ptci for ptci in pairwise_ptci_vals if not is_none_or_nan(ptci)]
+    if not quiet:
+        print "Returning %s ptci_vals." % (len(pairwise_ptci_vals))
+        
+    return pairwise_ptci_vals
+
+def get_null_pairwise_ptci_distributions(graphHandler,graphBuilder,n_way_ortho_table,ortho_parser,reps=50,kind='rpd',quiet=True,w_min=1.0,w_max=1.1):
+    """
+    Function to calculate and store RANDOMIZED 1-to-1 pairwise PTCI values in the graph edges to generate many NULL distributions
+    """
+    null_paired_ptci_distributions = []
+
+    for rep in range(reps):
+        # scramble edges for this rep and set new r&p vals
+        reset_random_edges(graphHandler,graphBuilder,n_way_ortho_table,ortho_parser)
+        graphHandler.measure_relations()
+        
+        # do prep
+        null_edges = graphHandler.edge_dict.values()
+        null_r_and_p_values = get_edge_r_and_p_vals(null_edges,quiet)
+        null_r_values = [null_r_and_p_values[i][0] for i in range(len(null_r_and_p_values))]
+        null_z_stats = m.get_z_score_stats(null_r_values)
+        set_z_vals(graphHandler,null_z_stats,use_center='median')
+        
+        # calculate null ptci vals
+        null_pairwise_ptci_vals = get_pairwise_ptci_vals(null_edges,kind,quiet,w_min,w_max)
+        
+        # collect null ptci distribution
+        null_paired_ptci_distributions.append(null_pairwise_ptci_vals)
+        
+    
+    return null_paired_ptci_distributions
+
+
+def get_mean_ptcis(graphHandler,n_way_ortho_table,kind='rpd',quiet=True,w_min=1.0,w_max=1.1):
+    """
+    returns list of n-way averaged PTCI values for N-way ortholog subgraphs if and only if 
+    all edges successfully generated non-None value PTCI results.
+    
+    Stores mean ptci in ortho_edge.data.meanPTCI
+    """
+    
+    # dictionary of nodes/edges indexed by gene names
+    node_dict = graphHandler.node_dict
+    edge_dict = graphHandler.edge_dict
+    
+    graph = graphHandler.graph 
+    
+    mean_ptcis = []
+    
+    # calculate all pairwise combinations of indexes
+    # so that each ortho-edge of n-way orthos are obtained
+    
+    index_combos = list(xpermutations.xuniqueCombinations(range(len(n_way_ortho_table.columns)),2))
+    
+    
+    for node_list in n_way_ortho_table.itertuples():
+        
+        node_list = node_list[1:]
+        
+        ortho_edges = []
+        for i in index_combos:
+            key = tuple(sorted([node_list[i[0]],node_list[i[1]]]))
+            
+            try:
+                ortho_edges.append(edge_dict[key])
+            except KeyError:
+                break
+                
+        ptcis = [get_ptci(edge,kind,w_min,w_max) for edge in ortho_edges]
+        
+        try:
+            mean_ptci = np.mean(ptcis)
+            
+            #store mean ptci in each member edge
+            for ortho_edge in ortho_edges:
+                ortho_edge.data.meanPTCI = mean_ptci
+                
+            mean_ptcis.append(mean_ptci)
+        except TypeError:
+            pass
+
+    return mean_ptcis
+
+
+
+def set_z_vals(graphHandler,z_stats,use_center='median'):
+    """
+    Function to use z-score stats to calculate and store z-score converted r-values in the gFunc graph
+    """
+    z_stats = {'mean':z_stats[0],'median':z_stats[1],'stdv':z_stats[2]}
+    
+    center = z_stats[use_center]
+    stdv   = z_stats['stdv']
+    
+    def z_val(r_val,center,stdv):
+        return  (r_val - center) / stdv
+    
+    edges = graphHandler.edge_dict.values() 
+    for edge in edges:
+        try:
+            edge.data.z_val = z_val(edge.data.r_val,center,stdv)
+            
+        except (TypeError,AttributeError) as exc:
+            if 'TypeError' in str(exc):
+                edge.data.z_val = None
+            elif 'AttributeError' in str(exc):
+                edge_correlation(edge)
+                if edge.data.r_val == None:
+                    edge.data.z_val = None
+                else:
+                    edge.data.z_val = z_val(edge.data.r_val,center,stdv)
+            
+
+
+def get_null_mean_ptci_distributions(graphHandler,graphBuilder,n_way_ortho_table,ortho_parser,reps=50,kind='rpd',quiet=True,w_min=1.0,w_max=1.1):
+    """
+    Function to calculate RANDOMIZED mean PTCI values to generate many NULL distributions
+    """
+    null_mean_ptci_distributions = []
+
+    for rep in range(reps):
+        # scramble edges for this rep and set new r&p vals
+        reset_random_edges(graphHandler,graphBuilder,n_way_ortho_table,ortho_parser)
+        graphHandler.measure_relations()
+        
+        
+        # do prep
+        null_edges = graphHandler.edge_dict.values()
+        null_r_and_p_values = get_edge_r_and_p_vals(null_edges,quiet)
+        null_r_values = [null_r_and_p_values[i][0] for i in range(len(null_r_and_p_values))]
+        null_z_stats = m.get_z_score_stats(null_r_values)
+        set_z_vals(graphHandler,null_z_stats,use_center='median')
+        
+        # calculate null ptci vals
+        null_mean_ptci_vals = calc_mean_ptcis(graphHandler,n_way_ortho_table,kind,quiet,w_min,w_max)
+        
+        # collect null ptci distribution
+        null_mean_ptci_distributions.append(null_mean_ptci_vals)
+        
+    
+    return null_mean_ptci_distributions
+
+
+
 
 
 ######################## START CALC  PTCI LAND ######################################
@@ -214,11 +397,10 @@ def calc_ptci_rsrd(gFunc_edge,w_min=1.0,w_max=1.1):
     """
     try:
         r_val_expn = gFunc_edge.data.r_val
-        p_val_expn = gFunc_edge.data.p_val
         r_val_tfbs = gFunc_edge.data.tfbs_vector_similarity.r_val
         d_val,d_min,d_max = gFunc_edge.data.divergence
         
-        return (r_val_expn + scale_r_val(r_val_tfbs)) * scale_the_d(d_val,d_min,d_max,w_min,w_max) 
+        return (r_val_expn + (r_val_tfbs/2.0) * scale_the_d(d_val,d_min,d_max,w_min,w_max)) 
     
     except (TypeError,AttributeError):
         return None
--- src/gfunc/dev/motifs.py
+++ src/gfunc/dev/motifs.py
@@ -16,26 +16,92 @@ from gfunc.motifs import process_MOODS_results
 from spartan.utils.files import ParseFastA
 
 
-def get_score_dataframe(processed_MOODS_results):
+def get_motif_names_from_meme_file(meme_file_path):
+    """
+    """
+    motif_names = []
+    
+    for line in open(meme_file_path,'rU'):
+        if line.startswith('MOTIF'):
+            motif_names.append(line.strip().split()[1])
+    
+    return motif_names
+            
+
+def get_seq_names_from_steme(steme_pwm_scan_seqs):
+    """
+    """
+    pos_name_dict = {}
+    index = 0
+    for line in open(steme_pwm_scan_seqs,'rU'):
+        line = line.strip().split(',')
+        pos_name_dict[int(index)] = line[1]
+        index += 1
+    
+    return pos_name_dict
+
+def process_STEME_SCAN_results(steme_pwm_scan_out,steme_pwm_scan_seqs,motif_file_meme):
+    """
+    *GIVEN:*
+        * ``steme_pwm_scan_out``: csv file from steme-pwm-scan (steme-pwm-scan.out)
+        * ``steme_pwm_scan_seqs``: csv file from steme-pwm-scan (steme-pwm-scan.seqs)
+        * ``motif_file_meme``: meme formated PWM file used to generate ``steme-pwm-scan.out``
+    *DOES:*
+        * converts ``steme_pwm_scan_out`` into a three key'd multi-level dict as follows:
+          ``process_STEME_SCAN_results_dict[SeqName][motifName][location] = score``
+    *RETURNS:*
+        * ``process_STEME_SCAN_results_dict``
+    """
+    dict_tree = lambda: defaultdict(dict_tree)
+    processed_steme_result_dict = dict_tree()
+    
+    
+    motif_names = get_motif_names_from_meme_file(motif_file_meme)
+    seq_pos_name = get_seq_names_from_steme(steme_pwm_scan_seqs)
+    
+    # initialize process_STEME_SCAN_results_dict with seq_names and motif_names
+    for seq_index,seq_name in seq_pos_name.iteritems():
+        for motif in motif_names:
+            processed_steme_result_dict[seq_name][motif]
+    
+    for line in open(steme_pwm_scan_out,'rU'):
+        try:
+            fields = line.strip().split(',')
+            m_name = fields[0]
+            s_name = seq_pos_name[int(fields[2])]
+            location = int(fields[3])
+            score  = float(fields[5])
+            if score > 0.5:
+                processed_steme_result_dict[s_name][m_name][location] = score
+        except KeyError:
+            print fields
+            raise
+    return processed_steme_result_dict
+
+
+
+def get_score_dataframe(processed_MOTIF_SEARCH_results,search_kind='MOODS'):
     """
     :returns: pandas.DataFrame obj with seq_names as row headings and
-    motif_names as column headings; values are sum of the positive log-odds
-    scores for a particular motif_name in that seq_name.
+    motif_names as column headings; values are sum of the filtered_scores
+    for a particular motif_name in that seq_name.
     """
-    motif_names = sorted(processed_MOODS_results.values()[0].keys())
+    score_threshold_dict = {'MOODS':0,
+                            'STEME_SCAN':0.5}
+    motif_names = sorted(processed_MOTIF_SEARCH_results.values()[0].keys())
     
     dict_of_series_objects = {}  
     
-    for seq_name in sorted(processed_MOODS_results.keys()):
+    for seq_name in sorted(processed_MOTIF_SEARCH_results.keys()):
         # intialize pandas.Series(data, index) object and store in dict_of_series_objects
-        data = []
+        motif_data = []
         for motif_name in motif_names:
-            # for each motif sum all positive scores (log-odds)
+            # for each motif sum all scores over threshold (MOODS = log-odds;0  STEME_SCAN = Z;0.5)
             # negative log-odds mean that the site has less than even odds of being the motif given the background
-            pos_log_odds = [ x for x in processed_MOODS_results[seq_name][motif_name].values() if x > 0 ]
-            data.append(np.sum(pos_log_odds))
+            filtered_scores = [ x for x in processed_MOTIF_SEARCH_results[seq_name][motif_name].values() if x > score_threshold_dict[search_kind] ]
+            motif_data.append(np.sum(filtered_scores))
             
-        dict_of_series_objects[seq_name] = pandas.Series(data=data, index=motif_names)
+        dict_of_series_objects[seq_name] = pandas.Series(data=motif_data, index=motif_names)
         
     return pandas.DataFrame.from_dict(dict_of_series_objects,orient='index')
 
@@ -88,3 +154,24 @@ def get_and_save_motif_hits(motif_obj,fasta_path,species,out_path):
     
     # write results to out_path as TSV
     save_dataframe_to_motif_table(dataframe=df_norm_median, species=species, out_path=out_path)
+    
+def norm_and_save_STEME_SCAN(steme_pwm_scan_out,steme_pwm_scan_seqs,motif_file_meme,species,out_path):
+    """
+    """
+    processed_STEME_SCAN_results = process_STEME_SCAN_results(steme_pwm_scan_out,steme_pwm_scan_seqs,motif_file_meme)
+    df = get_score_dataframe(processed_STEME_SCAN_results,search_kind='STEME_SCAN')
+    
+    # standardize the scores
+    df_norm_median = (df - df.median())/df.std()
+    
+    # write results to out_path as TSV
+    save_dataframe_to_motif_table(dataframe=df_norm_median.fillna(0), species=species, out_path=out_path)    
+    
+    
+#for sn,ss in cq_seqs.iteritems():
+    #try:
+        #start = ss.find(seqs.revcomp('CCCAAAATAG'))
+        #if start != -1:
+            #print '%s: %s' % (sn,start) 
+    #except IndexError:
+        #print '%s: length %s' % (sn,len(ss))
